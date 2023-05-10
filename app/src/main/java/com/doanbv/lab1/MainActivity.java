f980b9054d7ff6b838806bdcdb56f21ace523129
doanbv1 <133104362+doanbv1@users.noreply.github.com>
2023-05-10 18:14:02 +0700

    Initial commit

---
+++ app/src/main/java/com/doanbv/lab1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.doanbv.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+//    boolean flagBg = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        LinearLayout mainLauout = findViewById();
+    }
+}
